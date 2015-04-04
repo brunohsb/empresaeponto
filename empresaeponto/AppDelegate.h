c4117d74126fcf648bde15e3d3a7c28d660b2944
Rafael Assis <rafazassis@gmail.com>
2015-04-04 10:44:46 -0300

    Initial Commit

---
+++ empresaeponto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  empresaeponto
+//
+//  Created by Rafael Assis on 4/4/15.
+//  Copyright (c) 2015 Curso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ empresaeponto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  empresaeponto
+//
+//  Created by Rafael Assis on 4/4/15.
+//  Copyright (c) 2015 Curso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
