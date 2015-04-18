6b4c5f68f308a7450471ad98e96c8c2041fc145b
brunohsb <brunohenriquesousabarbosa@hotmail.com.br>
2015-04-18 09:56:36 -0300

    Merge branch 'S01-01' of https://github.com/brunohsb/empresaeponto
    Conflicts:
    	empresaeponto/Base.lproj/Main.storyboard

---
+++ empresaeponto/Usuario.h
@@ -0,0 +1,17 @@
+//
+//  Usuario.h
+//  empresaeponto
+//
+//  Created by its4aprendiz on 11/04/15.
+//  Copyright (c) 2015 Curso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Usuario : NSObject
+
+@property(strong,nonatomic)NSString *Usuario;
+
+@property (strong, nonatomic) NSString *Senha;
+
+@end
